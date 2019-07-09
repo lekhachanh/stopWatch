43cd83a468dbdb04ef2e1c037c719ed34cf34cf8
lekhachanh <lekhachanh123@gmail.com>
2019-07-09 16:15:24 +0700

    first commit

---
+++ src/StopWatch.java
@@ -0,0 +1,32 @@
+public class StopWatch {
+    private long startTime;
+    private long endTime;
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
+    public void start () {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public void stop () {
+        this.endTime = System.currentTimeMillis();
+    }
+
+
+    public long getElapsedTime() {
+        return (this.getEndTime() - this.getStartTime());
+    }
+}
